e1119a77d084b7bf8d056bd9356639d63b4a24ba
doubibobo <doubibobolalala@gmail.com>
2019-03-18 09:56:10 +0800

    something

--- app.py
+++ app.py
@@ -626,4 +626,23 @@ def test_question_lists():
 
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
-    app.run(threaded=True, debug=False, host='0.0.0.0', port=8090)
+    app.run(threaded=True, debug=False, host='0.0.0.0', port=80)
+
+
+server {
+    listen  80;
+    server_name 95.179.160.135;
+    location / {
+        include         uwsgi_params;
+        uwsgi_pass      127.0.0.1:8080;
+        uwsgi_param     UWSGI_PYHOME /root/retest/venv;
+        uwsgi_param     UWSGI_CHDIR  /root/retest;
+        uwsgi_param     UWSGI_SCRIPT manage:app;
+    }
+    error_page 404 /404.html;
+        location = /40x.html {
+    }
+    error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+libreoffice 安装
+安装字体：https://blog.csdn.net/qq_30554229/article/details/80093894
+
+https://blog.csdn.net/qq_41090453/article/details/83451321
